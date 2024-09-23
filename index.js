9fb31374f51667fc08468192e17dab0540825e23
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-09-23 07:28:44 -0300

    Comportamentos de classe

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor)
+            this.saldo -= valor;
+    }
 }
 
 const cliente1 = new Cliente();
 
 cliente1.nome = 'Ricardo';
 cliente1.cpf = '11122233344';
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 
 cliente2.nome = 'Alice';
 cliente2.cpf = '55566677788';
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, '\n', cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 0;
+
+contaCorrenteRicardo.saldo = 100;
+
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.sacar(200);
+
+console.log(contaCorrenteRicardo.saldo);
+
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.agencia = 1001;
+contaCorrenteAlice.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
