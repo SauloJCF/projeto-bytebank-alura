ddfc90772f22e152abe6c26d47b1d76d7844e1b2
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-09-24 07:17:51 -0300

    Métodos com retorno

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 class Cliente {
-    nome;
-    cpf;
+  nome;
+  cpf;
 }
 
 class ContaCorrente {
-    agencia;
-    _saldo = 0;
+  agencia;
+  _saldo = 0;
 
-    sacar(valor) {
-        if (this._saldo >= valor)
-            this._saldo -= valor;
-    }
+  sacar(valor) {
+    if (this._saldo < valor) return;
 
-    depositar(valor) {
-        if (valor > 0)
-            this._saldo += valor;
-    }
+    this._saldo -= valor;
+
+    return valor;
+  }
+
+  depositar(valor) {
+    if (valor <= 0) return;
+
+    this._saldo += valor;
+
+    return valor;
+  }
 }
 
 const cliente1 = new Cliente();
 
-cliente1.nome = 'Ricardo';
-cliente1.cpf = '11122233344';
+cliente1.nome = "Ricardo";
+cliente1.cpf = "11122233344";
 
 const cliente2 = new Cliente();
 
-cliente2.nome = 'Alice';
-cliente2.cpf = '55566677788';
+cliente2.nome = "Alice";
+cliente2.cpf = "55566677788";
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -35,8 +41,9 @@ contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(300);
 contaCorrenteRicardo.depositar(-1);
 
-contaCorrenteRicardo.sacar(50);
-contaCorrenteRicardo.sacar(200);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
 
