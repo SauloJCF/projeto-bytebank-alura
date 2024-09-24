89a22bf059d263df9da097c8de4e3f940942cef8
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-09-24 07:10:14 -0300

    Altera para convenção campos privados

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    #saldo = 0;
+    _saldo = 0;
 
     sacar(valor) {
-        if (this.#saldo >= valor)
-            this.#saldo -= valor;
+        if (this._saldo >= valor)
+            this._saldo -= valor;
     }
 
     depositar(valor) {
         if (valor > 0)
-            this.#saldo += valor;
+            this._saldo += valor;
     }
 }
 
