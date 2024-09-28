15ee6b32c7e8d755a08fde97120a7b2118675545
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-09-28 16:00:15 -0300

    Composição de classes

--- index.js
+++ index.js
@@ -11,19 +11,16 @@ const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "55566677788";
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrente1 = new ContaCorrente();
+contaCorrente1.agencia = 1001;
+contaCorrente1.depositar(500);
+contaCorrente1.cliente = cliente1;
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(300);
-contaCorrenteRicardo.depositar(-1);
+const contaCorrente2 = new ContaCorrente();
+contaCorrente2.agencia = 1001;
+contaCorrente2.cliente = cliente2;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
+contaCorrente1.transferir(300, contaCorrente2);
 
-console.log(valorSacado);
-
-console.log(contaCorrenteRicardo);
-
-const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteAlice.agencia = 1001;
-contaCorrenteAlice.saldo = 0;
+console.log(contaCorrente1);
+console.log(contaCorrente2);
